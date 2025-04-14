c9b96412f6b90e519ea0094c539eb557284613a9
Yush-sudo <otero.joshuacastro649@gmail.com>
2025-04-14 11:11:57 +0800

    Add files via upload

--- routes/intrusionAlert.js
+++ routes/intrusionAlert.js
@@ -3,12 +3,10 @@ const express = require('express');
 module.exports = (wss) => {
   const router = express.Router();
 
-  // POST /api/intrusion-alert
   router.post('/intrusion-alert', (req, res) => {
     const alert = req.body;
     console.log("📡 Received Intrusion Alert:", alert);
 
-    // Broadcast to all WebSocket clients
     wss.clients.forEach(client => {
       if (client.readyState === 1) {
         client.send(JSON.stringify({
